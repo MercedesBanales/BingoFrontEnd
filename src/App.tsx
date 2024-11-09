04ebcb12e9c53492c3b7c9e0c411966c5ff24b38
MercedesBanales <mercibanales@hotmail.com>
2024-11-08 22:02:20 -0300

    basic login form and page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage.tsx';
 
 const App = () => {
     return (
-        <div>
-        <h1>Hello, World!</h1>
-        </div>
+        <BrowserRouter>
+            <div>
+                <Routes>
+                    <Route path="/login" element={<LoginPage />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
     );
 }
 
---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Formik, Form} from 'formik';
+import Input from './Input.tsx';
+import { LoginSchema } from '../schemas/LoginSchema.tsx';
+import { LoginValue } from '../types/LoginValue.tsx';
+
+const LoginForm = () => {
+    const handleSubmit = (values: { email: string, password: string }) => {
+    }
+
+    return (
+        <Formik
+            initialValues={{ email: '', password: '',}}
+            validationSchema={LoginSchema}
+            onSubmit={(
+                values: { email: string, password: string }) => {
+                handleSubmit(values as { email: string, password: string });
+            }}
+        >
+            {
+                <Form className="flex flex-col items-center w-2/4 gap-8">
+                <div className="flex flex-col w-full gap-4">
+                    <Input<LoginValue> name="email" label="" type="email" placeholder="johndoe@hotmail.com" />
+                    <Input<LoginValue> name="password" label="" type="password" placeholder='*******' />
+                </div>
+                <button className="text-white bg-violet-400 rounded-3xl px-12 py-2 w-fit shadow-md hover:font-semibold hover:shadow-lg" type="submit">Login</button>
+            </Form>
+            }
+        </Formik>
+    )
+}
+
+export default LoginForm;
