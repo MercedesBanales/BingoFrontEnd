99c9e9ed7da0f034dfe1e3cfaa9d6f7b3de35d2f
MercedesBanales <mercibanales@hotmail.com>
2024-11-08 23:52:04 -0300

    created error dialog

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import LoginForm from '../components/LoginForm.tsx';
+import ErrorDialog from '../components/ErrorDialog.tsx';
 
 const LoginPage = () => {
     const [error, setError] = useState<string | null>(null);
@@ -8,7 +9,7 @@ const LoginPage = () => {
     return (
         <div className="flex flex-col items-center gap-24 justify-center w-full">
             <h1 className="font-bold text-4xl">Welcome</h1>
-            {error && <p>{error}</p>}
+            {error && <ErrorDialog error={error} onClose={() => {setError('')}}/>}
             <LoginForm  setError={setError}/>
         </div>
         
