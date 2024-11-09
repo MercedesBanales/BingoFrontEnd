53f8ab76fb7e2523e9f247f86b392001a2d82571
MercedesBanales <mercibanales@hotmail.com>
2024-11-09 13:07:16 -0300

    created navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,35 @@
 import './output.css';
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import LoginPage from './pages/LoginPage.tsx';
 import HomePage from './pages/HomePage.tsx';
+import NavigationBar from './components/NavigationBar.tsx';
 
 const App = () => {
     return (
         <BrowserRouter>
-            <div className="flex justify-center items-center h-fill w-full">
-                <Routes>
-                    <Route path="/login" element={<LoginPage />} />
-                    <Route path="/home" element={<HomePage />} />
-                </Routes>
-            </div>
+            <AppRoutes />
         </BrowserRouter>
     );
-}
+};
+
+const AppRoutes = () => {
+    const location = useLocation();
+
+    return (
+        <>
+        {location.pathname !== '/login' && <NavigationBar />}
+
+        <div className="flex justify-center items-center h-full w-full">
+            {/* Conditionally render NavigationBar */}
+            
+            <Routes>
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/home" element={<HomePage />} />
+            </Routes>
+        </div>
+        </>
+    );
+};
 
 export default App;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../store';
+import NavigationBar from '../components/NavigationBar.tsx';
 
 
 const HomePage = () => {
