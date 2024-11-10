aebd1ecbc245205debf22079e0154612034ad66c
MercedesBanales <mercibanales@hotmail.com>
2024-11-09 23:12:22 -0300

    fixed card component visuals

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,19 +10,22 @@ const Card = ({ card }) => {
     };
 
     return (
-        <div className="grid grid-cols-5 gap-2">
+        <div className="flex flex-col items-center justify-center gap-6 bg-indigo-100 p-4 rounded-md">
+            <p className="text-black text-5xl font-medium">BINGO 75</p>
+            <div className="grid grid-cols-5 gap-2">
         {card.map((row: number[], rowIndex: number) => (
             row.map((number: number, colIndex: number) => (
                 <button
                     key={`${rowIndex}-${colIndex}`}
                     onClick={() => handleButtonClick(number)}
-                    className="p-4 bg-indigo-100 text-black rounded hover:bg-indigo-700 hover:text-white"
+                    className="p-4 bg-white text-black rounded hover:bg-indigo-700 hover:text-white"
                 >
                     {rowIndex===2 && colIndex===2 ? 'FREE' : number}
                 </button>
             ))
         ))}
     </div>
+        </div>
     )
 
 };
